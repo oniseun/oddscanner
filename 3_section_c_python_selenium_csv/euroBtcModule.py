b68865e1ff86d42fcde6620e783610bc9ea88310
Victor Oni <seunoni34@gmail.com>
2023-02-09 18:13:20 +0000

    adjust days so it does not exceed maximum available table rows

--- 3_section_c_python_selenium_csv/euroBtcModule.py
+++ 3_section_c_python_selenium_csv/euroBtcModule.py
@@ -14,8 +14,9 @@ class EuroBTC:
         op = webdriver.ChromeOptions()
         op.add_argument('headless')
         driver = webdriver.Chrome(options=op)
+        print('Opening url "' + self.url +'"..')
         driver.get(self.url)
-        print('clicking on "Accept All"..')
+        print('done! \nclicking on "Accept All"..')
         agreeButton = WebDriverWait(driver, 10).until(
                 EC.presence_of_element_located((By.CSS_SELECTOR, 'button[name="agree"]'))
             ).click()
@@ -28,7 +29,8 @@ class EuroBTC:
         tableRows = driver.find_elements(By.CSS_SELECTOR, 'table[data-test="historical-prices"] tbody tr')
 
         rates = []
-        for x in range(days):
+        adjustedDays = min(days, len(tableRows))
+        for x in range(adjustedDays):
             row = tableRows[x]   
             cols = row.find_elements(By.TAG_NAME, "td") 
             date, close = cols[0].text, cols[4].text 
@@ -36,7 +38,7 @@ class EuroBTC:
 
         driver.close()
         self.rates = rates
-        print('EUR/BTC Rates fetched successfully from ' + rates[days - 1][0] + ' to ' + rates[0][0])
+        print('EUR/BTC Rates fetched successfully from ' + rates[adjustedDays - 1][0] + ' to ' + rates[0][0])
 
     def saveRates(self, csvFileName):
         import os
